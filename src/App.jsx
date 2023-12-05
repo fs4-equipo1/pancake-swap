bcd47f7e364f79ac0bff8bcde608207024a1c5ca
Manuel <manu.benitez1@hotmail.com>
2023-12-05 11:51:26 +0100

    container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Boton from "./componentes/Boton/Boton";
-
+import styles from "./styles/pages/home.module.scss";
 function App() {
   return (
-    <div>
-      <Boton texto={"Transparent"} isTransparent={true} />
-      <Boton texto={"Blue"} isBlue={true} />
-      <Boton texto={"White"} isWhite={true} />
+    <div className={styles.container}>
+      <div>
+        <Boton texto={"Transparent"} isTransparent={true} />
+        <Boton texto={"Blue"} isBlue={true} />
+        <Boton texto={"White"} isWhite={true} />
+      </div>
+      <div>
+        <Boton texto={"Transparent"} isTransparent={true} />
+        <Boton texto={"Blue"} isBlue={true} />
+        <Boton texto={"White"} isWhite={true} />
+      </div>
     </div>
   );
 }
