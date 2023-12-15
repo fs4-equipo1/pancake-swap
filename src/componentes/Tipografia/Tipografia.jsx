343502638f0d36ec10a4514bd1108389c760be79
manuelbenitez <manu.benitez1@hotmail.com>
2023-12-15 13:50:12 +0100

    feat: tipografia

---
+++ src/componentes/Tipografia/Tipografia.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./Tipografia.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+/**
+ *
+ * @param {string} color - Color del Texto
+ * @param {string} texto
+ * @returns
+ */
+
+const Tipografia = ({
+  color,
+  texto,
+  isTitleLarge,
+  isTitle,
+  isSubtitleLarge,
+  isSubtitle,
+  isBodyLarge,
+  isBody,
+}) => {
+  const classNames = cx({
+    titleLarge: isTitleLarge,
+    title: isTitle,
+    subtitleLarge: isSubtitleLarge,
+    subtitle: isSubtitle,
+    bodyLarge: isBodyLarge,
+    body: isBody,
+  });
+  return (
+    <h1 className={classNames} style={{ color: `var(${color})` }}>
+      {texto}
+    </h1>
+  );
+};
+
+export default Tipografia;
