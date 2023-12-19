0e87a21401c2a1efa8598df70bdbd092ca7f9d88
laura <lauraespanyol95@gmail.com>
2023-12-20 00:08:05 +0100

    Añadí la data de las ecosystem cards

--- src/EcosystemCard/EcosystemCardsData.js
+++ src/EcosystemCard/EcosystemCardsData.js
@@ -30,4 +30,58 @@ const EcosystemCardsData = [
     content: "Buy crypto with your preferred currency and payment method",
     button: "Buy Now"
     },
+    {id: 2,
+    imgsrc: "#",
+    title: "Farm",
+    content: "Stake LP tokens, harvest CAKE",
+    button: "Stake Now"
+    },
+    {id: 2,
+    imgsrc: "#",
+    title: "Pools",
+    content: "Stake CAKE, earn various rewards",
+    button: "Stake Now"
+    },
+    {id: 2,
+    imgsrc: "#",
+    title: "Liquid Staking",
+    content: "Earn rewards while retaining asset flexibility",
+    button: "Add Now"
+    },
+    {id: 2,
+    imgsrc: "#",
+    title: "Simple Staking",
+    content: "Earn rewards hassle-free with single-sided staking",
+    button: "Stake Now"
+    },
+    {id: 3,
+    imgsrc: "#",
+    title: "Prediction",
+    content: "Forecast token prices within minutes",
+    button: "Try Now"
+    },
+    {id: 3,
+    imgsrc: "#",
+    title: "Pancake Protectors",
+    content: "Immersive PvP & PvE tower-defense GameFi",
+    button: "Play Now"
+    },
+    {id: 3,
+    imgsrc: "#",
+    title: "Lottery",
+    content: "Enter for a chance to win CAKE prize pools",
+    button: "Try Now"
+    },
+    {id: 3,
+    imgsrc: "#",
+    title: "Pottery",
+    content: "Stake CAKE, acquire pottery tickets, win prizes",
+    button: "Try Now"
+    },
+    {id: 3,
+    imgsrc: "#",
+    title: "NFT Marketplace",
+    content: "Trade unique NFTs on BNB Chain",
+    button: "Trade Now"
+    },
 ]
