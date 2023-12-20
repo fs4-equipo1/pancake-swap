7acf1968bd8fb9202ac9f19e3c3dc61501387dbf
LuisTM98 <luistortosam@gmail.com>
2023-12-20 10:33:33 +0100

    refactor y fortmateado

--- src/EcosystemCard/EcosystemCard.jsx
+++ src/EcosystemCard/EcosystemCard.jsx
@@ -3,36 +3,40 @@ import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
-export function Card({imageSrc, titleText, contentText, commandText}){
-    return (
-        <div>
-            <img src={imageSrc} alt="" />
-            <h3>{titleText}</h3>
-            <p>{contentText}</p>
-            <h4>{commandText}</h4>
-        </div>
-    )
+export function Card({ imageSrc, titleText, contentText, commandText }) {
+  return (
+    <div>
+      <img src={imageSrc} alt="" />
+      <h3>{titleText}</h3>
+      <p>{contentText}</p>
+      <h4>{commandText}</h4>
+    </div>
+  );
 }
 
-export function EcosystemCardsContainer({path, titulo, cardData, objectId}) {
+export function EcosystemCardsContainer({ path, titulo, cardData, objectId }) {
+  const filteredData = cardData.filter((obj) => obj.id === objectId);
 
-    const filteredData = cardData.filter((obj) => obj.id === objectId);
-    
-    return (
+  return (
     <div className={styles.discoverEcosystem}>
-        <div className={styles.ecosystemContainertrade}>
-            <img className={styles.ecosystemImg} src={path}/>
-                <div className={styles.ecosystemTrade}>
-                    <div className={styles.ecosystemTitle}>{titulo}</div>
-                    <div className={styles.cardcontainer}>
-                        {/*filteredData.forEach(data => {
-                            <Card imageSrc={data.imgsrc} titleText={data.title} contentText={data.content} commandText={data.button} />
-                        })*/}
-                        {filteredData.map(data => {
-                            return <Card imageSrc={data.imgsrc} titleText={data.title} contentText={data.content} commandText={data.button} />
-                        })}
-                    </div>
-                </div>
+      <div className={styles.ecosystemContainertrade}>
+        <img className={styles.ecosystemImg} src={path} />
+        <div className={styles.ecosystemTrade}>
+          <div className={styles.ecosystemTitle}>{titulo}</div>
+          <div className={styles.cardcontainer}>
+            {filteredData.map((data) => {
+              return (
+                <Card
+                  imageSrc={data.imgsrc}
+                  titleText={data.title}
+                  contentText={data.content}
+                  commandText={data.button}
+                />
+              );
+            })}
+          </div>
         </div>
-  </div>)
+      </div>
+    </div>
+  );
 }
