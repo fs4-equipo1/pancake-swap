65cedeffeafbc1eb95cf46e63f9721ed7460d0ab
Ebenitez03 <elianenbenitezd04@gmail.com>
2023-12-20 23:20:02 +0100

    refactor: Cambiadas pequeñas cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import Boton from "./componentes/Boton/Boton";
 import styles from "./styles/pages/home.module.scss";
+import FavoriteDex from "./componentes/FavoriteDex/FavoriteDex";
 function App() {
   return (
     <div className={styles.container}>
-      <div>
-        <Boton texto={"Transparent"} isTransparent={true} />
-        <Boton texto={"Blue"} isBlue={true} />
-        <Boton texto={"White"} isWhite={true} />
-      </div>
-      <div>
-        <Boton texto={"Transparent"} isTransparent={true} />
-        <Boton texto={"Blue"} isBlue={true} />
-        <Boton texto={"White"} isWhite={true} />
-      </div>
-      <div>
-        <favoriteDex></favoriteDex>
-      </div>
+        <FavoriteDex/>
     </div>
   );
 }
--- src/componentes/FavoriteDex/FavoriteDex.jsx
+++ src/componentes/FavoriteDex/FavoriteDex.jsx
@@ -1,33 +1,46 @@
-import Boton from "./componentes/Boton/Boton";
-import Tipografia from "./componentes/Tipografia/Tipografia";
-import styles from "./componentes/FavoriteDex/FavoriteDex.module.scss";
+import Boton from "../Boton/Boton";
+import Tipografia from "../Tipografia/Tipografia";
+import styles from "./FavoriteDex.module.scss";
 import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
 export function FavoriteDex() {
   return (
-    <div>
-      <div className={styles.textContainer}>
+    <div className={styles.favoriteDex}>
+      <div className={styles.container}>
         <Tipografia
           color={"--colors-text"}
           texto={"Everyone's"}
           isTitleLarge
         ></Tipografia>
+        <div className={styles.separador}>
+          <Tipografia
+            color={"--colors-secondary"}
+            texto={"Favorite"}
+            isTitleLarge
+          />
+          <Tipografia color={"--colors-text"} texto={"DEX"} isTitleLarge />
+        </div>
+        <div className={styles.subtext}>
+          <Tipografia
+            color={"--colors-textSubtle"}
+            texto={
+              "Trade, earn, and own crypto on the all-in-one multichain DEX"
+            }
+            isSubtitleLarge
+          ></Tipografia>
+        </div>
+        <div className={styles.buttons}>
+          <Boton texto={"Connect wallet"} isBlue></Boton>
+          <Boton texto={"Trade now"} isTransparent></Boton>
+        </div>
       </div>
-      <div className={styles.subtext}>
-        <Tipografia
-          color={"--colors-secondary"}
-          texto={"Favorite"}
-          isTitleLarge
-        />
-        <Tipografia color={"--colors-text"} texto={"DEX"} isTitleLarge />
-      </div>
-      <div>
-        <Boton texto={"Connect wallet"} isBlue></Boton>
-        <Boton texto={"Trade now"} isTransparent></Boton>
-        <img></img>
-      </div>
+      <img
+        className={styles.img}
+        src="./Assets/Images/space-bunny.png"
+      ></img>
     </div>
   );
 }
+export default FavoriteDex;
