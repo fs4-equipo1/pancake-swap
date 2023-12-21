b1e38253fa833a1c45c0ffef15a0ad371c654340
Perlo1 <juancarlosmondejar6@gmail.com>
2023-12-21 19:44:35 +0100

    feat: new button

--- src/componentes/Boton/Boton.jsx
+++ src/componentes/Boton/Boton.jsx
@@ -3,12 +3,13 @@ import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
-const Boton = ({ texto, isTransparent, isBlue, isWhite, icon }) => {
+const Boton = ({ texto, isLearn, isTransparent, isBlue, isWhite, icon }) => {
   const className = cx({
     button: true,
     transparent: isTransparent,
     blue: isBlue,
     white: isWhite,
+    learn: isLearn,
   });
   return <button className={className}>{texto}{icon}</button>;
 };
