5874c8de49d92370a11b00d4de201c7157f25d02
ivandoza <ivandoza@gmail.com>
2024-01-07 19:10:42 +0000

    cambios en botones

--- src/componentes/NewsSwiper/NewsSwiper.jsx
+++ src/componentes/NewsSwiper/NewsSwiper.jsx
@@ -1,23 +1,28 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { NewsSwiperCardData } from "../../mocks/NewsSwiperCard.jsx";
+import { NewsSwiperCardData } from "../../mocks/NewsSwiperCard";
 import NewsSwiperCard from "../NewsSwiperCard/NewsSwiperCard";
 import "./NewsSwiper.scss";
 
 export default () => {
   return (
+    <div>
       <Swiper
         className="news-swiper-container"
         modules={[Navigation]}
-        navigation={true}
-        spaceBetween={50}
+        spaceBetween={40}
         slidesPerView={4}
+        navigation={{
+          nextElel: ".swiper-button-prev",
+          prevElel: ".swiper-button-next",
+        }}
       >
+        <div className="swiper-button-prev"></div>
+
         {NewsSwiperCardData.map((card, index) => (
-          <SwiperSlide key={index}>
+          <SwiperSlide className="news-swiper-slide" key={index}>
             <NewsSwiperCard
               image={card.image}
               from={card.from}
@@ -28,5 +33,7 @@ export default () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="swiper-button-next"></div>
+    </div>
   );
 };
