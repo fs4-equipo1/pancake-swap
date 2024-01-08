a36d08eecd249d3ae1c195772eef8438993ed13b
LuisTM98 <luistortosam@gmail.com>
2024-01-08 20:44:51 +0100

    removidos importes no usados

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,30 +1,12 @@
 import HeroSwiper from "./componentes/HeroSwiper/HeroSwiper";
-import imagenes from "../public/assets/imagenes";
 import FavoriteDex from "./componentes/FavouriteDex/FavoriteDex";
-import styles from "./styles/pages/home.module.scss";
-import { EcosystemCardsData } from "./EcosystemCard/EcosystemCardsData";
-import { EcosystemCardContainersContainer, EcosystemCardsContainer } from "./EcosystemCard/EcosystemCard";
-import Boton from "./componentes/Boton/Boton";
-import background from "./assets/background.png";
+import { EcosystemCardContainersContainer } from "./EcosystemCard/EcosystemCard";
 // import Tipografia from "./componentes/Tipografia/Tipografia";
-import LanguageDropdown from "./componentes/LanguageDropdown/LanguageDropdown";
-import { Icono } from "./componentes/Icono/Icono";
-import {
-  FaTwitter,
-  FaTelegram,
-  FaReddit,
-  FaInstagram,
-  FaGithub,
-  FaDiscord,
-  FaYoutube,
-} from "react-icons/fa";
 import Navbar from "./componentes/Navbar/Navbar";
 import Footer from "./componentes/Footer/Footer";
-import DexNow from "./componentes/DexNow/DexNow";
 import InfiniteSlider from "./componentes/InfiniteSlider/InfiniteSlider";
 import { JoinComunidad } from "./componentes/JoinComunidad/JoinComunidad";
 // import { CardComunidad } from "./componentes/JoinComunidad/CardComunidad";
-import ThemeToggle from "./componentes/ThemeToggle/ThemeToggle";
 import VerticalRightSlider from "./componentes/VerticalSlider/VerticalRightSlider";
 import VerticalLeftSlider from "./componentes/VerticalLogoSlider/VerticalLeftSlider";
 import SliderSeparator from "./componentes/SliderSeparator/SliderSeparator";
