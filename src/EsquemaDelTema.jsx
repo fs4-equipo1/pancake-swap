03ab4f412699ed67d47bb928889ed2a8f8ac2a7a
LuisTM98 <luistortosam@gmail.com>
2024-01-12 09:43:43 +0100

    refactor: removed explanation from App2.jsx, moved to explanation file EsquemaDelTema.jsx

--- src/EsquemaDelTema.jsx
+++ src/EsquemaDelTema.jsx
@@ -1,3 +1,16 @@
+//Las funciones ThemeToggle estaban en ThemeToggle.jsx y usaban un useState y un useEffect.
+//El UseEffect establece que ha de cambiar y como (el theme, que en ThemeToggle.jsx es una variable, y la
+//arrowfunction que setea el tema general de la pagina) y las dependencias, que esencialmente, es el objeto [Theme].
+//El objeto [theme] ha de ser fabricado mediante un useState en app2.jsx, y pasado como prop hasta donde haga falta (en
+//donde este usado el themetoggle, es decir, en navbar y en el footer) y de ahi ser pasado de nuevo como prop al ThemeToggle de cada uno.
+
+//El codigo no ha cambiado, unicamente se ha movido el sestate y el objeto Theme al app, de forma global. Los elementos capaces de variar
+//son pasados como props a los elementos padres de los dos themtoggles. De este modo, ambos tienen acceso simultaneo al tema de la pagina,
+//sin interferir uno con otro
+
+//Aqui un ejemplo visual
+
+
 function App2 (){
     
     function LaFuncionQueDejaElegirElTema(){
@@ -25,3 +38,4 @@ function ComponenteHijo ( {Theme} ) {
     return <div className={Theme === 'light' ? "DivModoClaro" : "DivModoOscuro"}></div>
     // aqui es donde hay algun componente, un div o algo, que necesita saber el tema de la pagina, por el motivo que sea
 }
+
