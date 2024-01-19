62fd29b1a24fd824773ae2f3f652da0651cbbc5f
manuelbenitez <manu.benitez1@hotmail.com>
2024-01-19 13:07:46 +0100

    feat: create store

---
+++ src/store/store.jsx
@@ -0,0 +1,9 @@
+import { action, createStore } from "easy-peasy";
+
+export const store = createStore({
+  todos: ["Create store", "Wrap application", "Use store"],
+
+  addTodo: action((state, payload) => {
+    state.todos.push(payload);
+  }),
+});
