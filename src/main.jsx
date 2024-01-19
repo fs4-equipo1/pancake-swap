e43a99f989e2473784afd85dfabe4e900b2b72ba
manuelbenitez <manu.benitez1@hotmail.com>
2024-01-19 13:10:28 +0100

    feat: wrap app y use store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
-import App from "./App.jsx"
+import App from "./App.jsx";
+import { StoreProvider } from "easy-peasy";
+import { store } from "./store/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
   </React.StrictMode>
 );
