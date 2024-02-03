9bf043c01ce1b4ec0d8b0ddb31cb772ddc32a20d
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-02-03 20:49:51 +0100

    refactor: made it responsive, added text in component

--- src/componentes/FavouriteDex/FavoriteDex.jsx
+++ src/componentes/FavouriteDex/FavoriteDex.jsx
@@ -1,69 +1,18 @@
-import { useEffect, useState } from "react";
 import Boton from "../Boton/Boton";
-import Tipografia from "../Tipografia/Tipografia";
 import styles from "./FavoriteDex.module.scss";
 import classNames from "classnames/bind";
+import Texto from "./Texto";
 
 const cx = classNames.bind(styles);
 
 export function FavoriteDex() {
-  const windoWidth = window.outerWidth;
-  const [isTitle, setIsTitle] = useState(false);
-  useEffect(() => {
-    const handleResize = () => {
-      setIsTitle(windoWidth <= 768 ? true : false);
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [windoWidth]);
   return (
     <div className={styles.favoriteDex}>
       <div className={styles.imgContainer}>
         <img className={styles.img} src="./Assets/Images/space-bunny.webp" />
       </div>
       <div className={styles.container}>
-        <div className={styles.textContainer}>
-          <div className={styles.title}>
-            <Tipografia
-              color={"--colors-text"}
-              texto={"Everyone's"}
-              isTitleLarge={!isTitle}
-              isTitle={isTitle}
-            ></Tipografia>
-            <div className={styles.separador}>
-              <Tipografia
-                color={"--colors-secondary"}
-                texto={"Favorite"}
-                isTitleLarge={!isTitle}
-                isTitle={isTitle}
-              />
-              <Tipografia
-                color={"--colors-text"}
-                texto={"DEX"}
-                isTitleLarge={!isTitle}
-                isTitle={isTitle}
-              />
-            </div>
-          </div>
-          <div className={styles.subtext}>
-            <Tipografia
-              color={"--colors-textSubtle"}
-              texto={"Trade, earn, and own crypto on the all-in-one"}
-              isSubtitle
-            ></Tipografia>
-            <Tipografia
-              color={"--colors-textSubtle"}
-              texto={"multichain DEX"}
-              isSubtitle
-            ></Tipografia>
-          </div>
-        </div>
+        <Texto/>
         <div className={styles.buttons}>
           <Boton texto={"Connect wallet"} isBlue></Boton>
           <Boton texto={"Trade now"} isTransparent></Boton>
---
+++ src/componentes/FavouriteDex/Texto.jsx
@@ -0,0 +1,127 @@
+import styles from "./FavoriteDex.module.scss";
+import Tipografia from "../Tipografia/Tipografia";
+import { useEffect, useState } from "react";
+
+export function Texto() {
+  const windoWidth = window.outerWidth;
+  const [isLaptop, setIsLaptop] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLaptop(windoWidth >= 1024 ? true : false);
+      setIsTablet(windoWidth <= 768 && windoWidth >= 426 ? true : false);
+      setIsMobile(windoWidth <= 425 ? true : false);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windoWidth]);
+  return (
+    <div>
+      {isMobile && (
+        <div className={styles.textContainer}>
+          <div className={styles.title}>
+            <Tipografia
+              color={"--colors-text"}
+              texto={"Everyone's"}
+              isSubtitleLarge
+            ></Tipografia>
+            <div className={styles.separador}>
+              <Tipografia
+                color={"--colors-secondary"}
+                texto={"Favorite"}
+                isSubtitleLarge
+              />
+              <Tipografia
+                color={"--colors-text"}
+                texto={"DEX"}
+                isSubtitleLarge
+              />
+            </div>
+          </div>
+          <div className={styles.subtext}>
+            <Tipografia
+              color={"--colors-textSubtle"}
+              texto={"Trade, earn, and own crypto on the all-in-"}
+              isBodyLarge
+            ></Tipografia>
+            <Tipografia
+              color={"--colors-textSubtle"}
+              texto={"one multichain DEX"}
+              isBodyLarge
+            ></Tipografia>
+          </div>
+        </div>
+      )}
+      {isTablet && (
+        <div className={styles.textContainer}>
+          <div className={styles.title}>
+            <Tipografia
+              color={"--colors-text"}
+              texto={"Everyone's"}
+              isTitle
+            ></Tipografia>
+            <div className={styles.separador}>
+              <Tipografia
+                color={"--colors-secondary"}
+                texto={"Favorite"}
+                isTitle
+              />
+              <Tipografia color={"--colors-text"} texto={"DEX"} isTitle />
+            </div>
+          </div>
+          <div className={styles.subtext}>
+            <Tipografia
+              color={"--colors-textSubtle"}
+              texto={"Trade, earn, and own crypto on the all-in-one"}
+              isSubtitle
+            ></Tipografia>
+            <Tipografia
+              color={"--colors-textSubtle"}
+              texto={"multichain DEX"}
+              isSubtitle
+            ></Tipografia>
+          </div>
+        </div>
+      )}
+      {isLaptop && (
+        <div className={styles.textContainer}>
+          <div className={styles.title}>
+            <Tipografia
+              color={"--colors-text"}
+              texto={"Everyone's"}
+              isTitleLarge
+            ></Tipografia>
+            <div className={styles.separador}>
+              <Tipografia
+                color={"--colors-secondary"}
+                texto={"Favorite"}
+                isTitleLarge
+              />
+              <Tipografia color={"--colors-text"} texto={"DEX"} isTitleLarge />
+            </div>
+          </div>
+          <div className={styles.subtext}>
+            <Tipografia
+              color={"--colors-textSubtle"}
+              texto={"Trade, earn, and own crypto on the all-in-one"}
+              isSubtitle
+            ></Tipografia>
+            <Tipografia
+              color={"--colors-textSubtle"}
+              texto={"multichain DEX"}
+              isSubtitle
+            ></Tipografia>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Texto;
