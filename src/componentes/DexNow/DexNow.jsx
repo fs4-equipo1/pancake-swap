7dabfe44db3952219476503ec029fd02f63022d3
Laura <lauraespanyol95@gmail.com>
2024-02-16 10:07:31 +0100

    DexNow from development

--- src/componentes/DexNow/DexNow.jsx
+++ src/componentes/DexNow/DexNow.jsx
@@ -2,8 +2,8 @@ import styles from "./DexNow.module.scss";
 
 import background from "../../assets/background.png";
 import Boton from "../Boton/Boton";
-import { t } from "i18next";
 import { useTranslation } from "react-i18next";
+
 const DexNow = ({ titleLarge }) => {
   return (
     <div>
@@ -23,11 +23,39 @@ export function DexNowSection() {
         backgroundPosition: "center",
       }}
     >
-      <div>
-        <DexNow titleLarge={t("FavouriteDEX")} />
-      </div>
-      <div>
-        <Boton texto={t("ConnectWallet")} isBlue={true} />
+      <div className={styles.contentContainer}>
+        <div className={styles.titleContainer}>
+          <div>
+            <DexNow titleLarge={t("FavouriteDEX")} />
+          </div>
+          <div>
+            <Boton texto={t("ConnectWallet")} isBlue={true} />
+          </div>
+        </div>
+        <img
+          className={styles.cakeRight}
+          src="../../Assets/Images/asset33.png"
+          alt="pancake-right"
+          width="160px"
+        ></img>
+        <img
+          className={styles.cakeLeft}
+          src="../../Assets/Images/asset32.png"
+          alt="pancake-left"
+          width="160px"
+        ></img>
+        <img
+          className={styles.rockLeft}
+          src="../../Assets/Images/asset35.png"
+          alt="rock-right"
+          width="160px"
+        ></img>
+        <img
+          className={styles.rockRight}
+          src="../../Assets/Images/asset34.png"
+          alt="rock-right"
+          width="160px"
+        ></img>
       </div>
     </div>
   );
