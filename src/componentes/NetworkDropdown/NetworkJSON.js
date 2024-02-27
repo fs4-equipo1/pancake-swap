c623571a746d060e0513d71d009ba03c7959d8fe
LuisTM98 <luistortosam@gmail.com>
2024-02-27 11:17:45 +0100

    Feat: added NetworkJSON.js first instance. Time to tweak and implement functionality

---
+++ src/componentes/NetworkDropdown/NetworkJSON.js
@@ -0,0 +1,52 @@
+export const networkJSON = {
+    "networks": [
+      {
+        "name": "BNB Chain",
+        "chainId": "0x38",
+        "rpcUrl": "https://bsc-dataseed.binance.org/",
+        "blockExplorerUrl": "https://bscscan.com/"
+      },
+      {
+        "name": "Ethereum",
+        "chainId": "0x1",
+        "rpcUrl": "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+        "blockExplorerUrl": "https://etherscan.io/"
+      },
+      {
+        "name": "Polygon",
+        "chainId": "0x89",
+        "rpcUrl": "https://rpc-mainnet.maticvigil.com/",
+        "blockExplorerUrl": "https://polygonscan.com/"
+      },
+      {
+        "name": "zkSync Era",
+        "chainId": "0x6E",
+        "rpcUrl": "https://api.zksync.io/jsrpc",
+        "blockExplorerUrl": "https://zkscan.io/"
+      },
+      {
+        "name": "Arbitrum One",
+        "chainId": "0xA4B1",
+        "rpcUrl": "https://arb1.arbitrum.io/rpc",
+        "blockExplorerUrl": "https://arbiscan.io/"
+      },
+      {
+        "name": "Linea Mainnet",
+        "chainId": "0x7A69",
+        "rpcUrl": "https://linea-api.linea.today",
+        "blockExplorerUrl": "https://explorer.linea.today/"
+      },
+      {
+        "name": "Base",
+        "chainId": "0x10",
+        "rpcUrl": "https://base-chain.minebase.org/rpc",
+        "blockExplorerUrl": "https://base-explorer.minebase.org/"
+      },
+      {
+        "name": "opBNB",
+        "chainId": "0x62",
+        "rpcUrl": "https://opbnbrpc.zenithchain.co",
+        "blockExplorerUrl": "https://opbscan.zenithchain.co/"
+      }
+    ]
+  }
